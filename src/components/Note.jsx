5dc0b32fc6e049d701f74b3f37bd145542030f1b
unknown <jesusdeleon.rd@gmail.com>
2023-06-22 18:12:43 -0600

    This is the First commit of the React project 1 377

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is a note Title</h1>
+            <p>This is a note content</p>
+        </div>
+    );
+}
+
+export default Note;
+
